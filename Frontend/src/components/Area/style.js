d763002b68bab7324b75160d02725c13d19bbeec
Danilo souza <danilo.souza@avecbrasil.com.br>
2022-04-09 06:41:05 -0300

    componente de area para montar espaco da table

---
+++ Frontend/src/components/Area/style.js
@@ -0,0 +1,12 @@
+import styled from 'styled-components';
+
+export const Container = styled.div`  
+  display: flex;
+  align-items: flex-start;
+  justify-content: center;
+  flex-grow: 8;
+  width: 100%;
+  cursor: pointer; 
+  
+  
+`;
